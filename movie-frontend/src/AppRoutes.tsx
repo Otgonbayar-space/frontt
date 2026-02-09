dce9b7c3887060aa4434d067085df2850795bf48
BACK-14 <back-14@Erxess-Mac-mini.local>
2026-02-09 20:55:45 +0800

    add card component

--- movie-frontend/src/AppRoutes.tsx
+++ movie-frontend/src/AppRoutes.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router";
-import App from "./App";
+import { MainLayout } from "./modules/app/MainLayout";
 
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/about" element={<div>about</div>} />
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<div>123</div>} />
+        <Route path="about" element={<div>about</div>} />
+      </Route>
     </Routes>
   );
 };
